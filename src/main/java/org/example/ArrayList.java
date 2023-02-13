b4807aedf9df735cda1d5f0931b9ee66651d8d2c
Tringapps-Ranjithkumar <ananthula.kumar@tringapps.com>
2023-02-13 14:35:49 +0530

    commit

--- src/main/java/org/example/ArrayList.java
+++ src/main/java/org/example/ArrayList.java
@@ -10,31 +10,31 @@ public class ArrayList{
         while(op>=1 && op<=4){
             logger.info("Select the below Operations \n 1. add \n 2. remove \n 3.Print elements \n 4.exit");
             op=sc.nextInt();
-            if(op==1){
-                logger.info("Enter the no of element to be added");
-                int n=sc.nextInt();
-                for(int i=0;i<n;i++){
-                    int element=sc.nextInt();
-                    myArray.add(element);
-                }
-            }
-            else if(op==2){
-                logger.info("enter the element to be removed");
-                int search=sc.nextInt();
-                if(myArray.contains(search)){
-                    myArray.remove(myArray.indexOf(search));
-                }
-            }
-            else if(op==3){
-                for(int i:myArray){
-                    System.out.println(i);
-                }
-            }
-            else if(op==4){
-                op=10;
-            }
-            else{
-                logger.info("something went wrong");
+            switch (op){
+                case 1:
+                    logger.info("Enter the no of element to be added");
+                    int n=sc.nextInt();
+                    for(int i=0;i<n;i++) {
+                        int element = sc.nextInt();
+                        myArray.add(element);
+                    }
+                    break;
+                    case 2:
+                    logger.info("enter the element to be removed");
+                    int search=sc.nextInt();
+                    if(myArray.contains(search)){
+                        myArray.remove(myArray.indexOf(search));
+                    }
+                    break;
+                case 3:
+                    for(int i:myArray){
+                        logger.info(i+"");
+                    }
+                    break;
+                case 4:
+                    op=10;
+                    break;
+
             }
         }
     }
